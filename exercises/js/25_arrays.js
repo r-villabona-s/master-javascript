f265c9090224d64b33b91c87843e567aa020ca50
rvillabonas <rvillabonas@correo.udistrital.edu.co>
2024-05-05 19:32:19 -0500

    exemple 25

--- exercises/js/25_arrays.js
+++ exercises/js/25_arrays.js
@@ -8,3 +8,14 @@ var paises = ["holanda", "alemania", "colomba", 52, true];
 
 var lenguajes = new Array("php", "js", "go", "java");
 console.log(lenguajes[0]);
+console.log("len ", lenguajes.length);
+
+var elemento = parseInt(prompt("Que elemento quiees ? ", 0));
+if (elemento > lenguajes.length - 1) {
+  alert("Introduce un numero entre 0 y " + parseInt(lenguajes.length - 1));
+} else {
+  alert("el lelemento seleccionado es " + lenguajes[elemento]);
+}
+
+document.write("<h1>Lenguajes de programación </h1>");
+array.forEach((element, index, data) => {});
